3b657a43bb4ac0286093a0039e62acaefc5deb64
_VIFEXTech <1290176185@qq.com>
2021-05-21 22:35:47 +0800

    Disable LV_BUILD_EXAMPLES

--- lv_layout_preview_v8/LVGL.Simulator/lv_conf.h
+++ lv_layout_preview_v8/LVGL.Simulator/lv_conf.h
@@ -491,7 +491,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 *==================*/
 
 /*Enable the examples to be built with the library*/
-#define LV_BUILD_EXAMPLES   1
+#define LV_BUILD_EXAMPLES   0
 
 /*--END OF LV_CONF_H--*/
 
